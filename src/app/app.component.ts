1fac0dacf50daa8ba56e35af0fdb792c07c61de8
AVER343 <ankushvermagb@gmail.com>
2020-06-29 11:56:41 +0530

    angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'password-gen';
+  password=''
+  lengthOfInput:number=0
+  checkboxOne=false
+  checkboxTwo=false
+  checkboxThree=false
+  onChangeUseLetters=()=>{
+    this.checkboxOne=!this.checkboxOne
+  } 
+  onChangeUseNumbers=()=>{
+    this.checkboxTwo=!this.checkboxTwo
+  } 
+  onChangeUseSymbols=()=>{
+    this.checkboxThree=!this.checkboxThree
+  } 
+  onButtonClick=()=>{
+    const numbers='0123456789'
+    const letters='abcdefghijklmnopqrstuvwxyz'
+    const symbols='-+=)(*&^%$#@!{}[]:;"<>/?`~'
+    let generatedPassword:string=''
+    let passwordToBe:string=''
+    if(this.checkboxOne){
+      generatedPassword=generatedPassword.concat(numbers)
+    }
+    if(this.checkboxTwo){
+      generatedPassword=generatedPassword.concat(letters)
+    }
+    if(this.checkboxThree){
+      generatedPassword=generatedPassword.concat(symbols)
+    }
+    let len = generatedPassword.length
+    let i:number
+    console.log(this.lengthOfInput)
+    for(i=0;i<this.lengthOfInput;i++)
+    {
+      let index = Math.floor(Math.random()*len)
+      passwordToBe = passwordToBe.concat(generatedPassword[index])
+    }
+    this.password = passwordToBe
+  }
+  onChange=(event)=>{
+    const parsedValue = parseInt(event.target.value)
+    if(!isNaN(parsedValue))
+    {
+      this.lengthOfInput = parsedValue
+    }
+    else{
+
+    }
+  }
 }
